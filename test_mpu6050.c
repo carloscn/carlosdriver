5f6ee5e6ba5ee56d392d431da60c469f4478419c
delvis <delvis@UBUNTU16.04>
2018-10-02 20:00:37 +0800

    test_mpu6050.c

--- test_mpu6050.c
+++ test_mpu6050.c
@@ -3,31 +3,35 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+short x_accel, y_accel, z_accel;
+short x_gyro, y_gyro, z_gyro;
+
 int main()
 {
 	char buffer[128];
+	short *time;
 	int in, out;
 	int nread;
-	unsigned int x_accel, y_accel, z_accel;
-	unsigned int x_gyro, y_gyro, z_gyro;
-
+	
 	in = open("/dev/MPU6050", O_RDONLY);
 	if (!in) {
 		printf("ERROR: %d, Open /dev/MPU6050 nod failed.\n", -1);
 		return -1;	
 	}	
 	nread = read(in, buffer, 12);
+	close(in);	
 	if (nread < 0) {
 		printf("ERROR: %d, A read error has occurred\n", nread);
 		return -1;	
 	}
-	close(in);
-	x_accel = buffer[1] + (buffer[0] << 8);
-	y_accel = buffer[3] + (buffer[2] << 8);
-	z_accel = buffer[5] + (buffer[4] << 8);	
-	x_gyro = buffer[7] + (buffer[6] << 8);
-	y_gyro = buffer[9] + (buffer[8] << 8);
-	z_gyro = buffer[11] + (buffer[10] << 8);	
+
+	time = (short*)buffer;
+	x_accel = *(time);
+	y_accel = *(time + 1);
+	z_accel = *(time + 2);
+	x_gyro =  *(time + 3);
+	y_gyro =  *(time + 4);
+	z_gyro =  *(time + 5);
 	printf("x accel is: %d \n", x_accel);
 	printf("y accel is: %d \n", y_accel);
 	printf("z accel is: %d \n", z_accel);	
